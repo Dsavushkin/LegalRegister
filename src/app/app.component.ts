cfa509d8c17e6bba17a90e44c5fb2c832dc771c9
savushkinandrew <savushkinandrew@gmail.com>
2019-07-26 10:58:55 +0300

    Dowloading main project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import {
+  map,
+  debounceTime,
+  distinctUntilChanged,
+  switchMap,
+  tap
+} from 'rxjs/operators';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+
+class SearchItem {
+  constructor(
+    public track: string,
+    public artist: string,
+    public link: string,
+    public thumbnail: string,
+    public artistId: string
+  ) {}
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'UClientRegister';
+
+class AppComponent {
+  private loading: boolean = false;
+  private results: Observable<SearchItem[]>;
+  private searchField: FormControl;
+
+  constructor(private itunes: SearchService) {}
+
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngOnInit() {
+    this.searchField = new FormControl();
+    this.results = this.searchField.valueChanges.pipe(
+      debounceTime(400),
+      distinctUntilChanged(),
+      tap(_ => (this.loading = true)),
+      switchMap(term => this.itunes.search(term)),
+      tap(_ => (this.loading = false))
+    );
+  }
+
+  doSearch(term: string) {
+    this.itunes.search(term);
+  }
 }
+
+class SearchService {
+}
+
+class AppComponent {
+  private loading: boolean = false;
+  private results: Observable<SearchItem[]>;
+  private searchField: FormControl;
+
+  constructor(private itunes: SearchService) {}
+
+  ngOnInit() {
+    this.searchField = new FormControl();
+    this.results = this.searchField.valueChanges.pipe(
+      debounceTime(400),
+      distinctUntilChanged(),
+      tap(_ => (this.loading = true)),
+      switchMap(term => this.itunes.search(term)),
+      tap(_ => (this.loading = false))
+    );
+  }
+
+  doSearch(term: string) {
+    this.itunes.search(term);
+  }
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: [SearchService]
 })
-export class AppModule { }
+
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
